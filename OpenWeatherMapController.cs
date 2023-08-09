d0ae5a0bc5961d905c2693656cf666f686a18134
Vanessa Putnam <vputnam99@gmail.com>
2023-08-09 16:31:25 -0700

    add parameters and build uri for one call api

--- OpenWeatherMapController.cs
+++ OpenWeatherMapController.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Net.Http;
 using System.Text;
+using System.Collections.Specialized;
+using System.Linq;
 
 namespace Dewy
 {
-    /// Two calsses here - one for database and one for porcessor ?
     public class OpenWeatherMapController
     {
-        private string OpenWeatherMapURL = Environment.GetEnvironmentVariable("OPEN_WEATHER_MAP_KEY"); // might move to start up? 
-        private string OpenWeatherMapKey = Environment.GetEnvironmentVariable("OPEN_WEATHER_MAP_URL");
+        private readonly string OpenWeatherMapURL = Environment.GetEnvironmentVariable("OPEN_WEATHER_MAP_KEY");
+        private readonly string OpenWeatherMapKey = Environment.GetEnvironmentVariable("OPEN_WEATHER_MAP_URL");
         private readonly HttpClient _httpClient;
         private string lat;
         private string lon;
@@ -19,17 +20,35 @@ namespace Dewy
             this._httpClient = httpClient;
             this.lat = lat;
             this.lon = lon;
+            _httpClient.BaseAddress = new Uri(OpenWeatherMapURL);
         }
 
         private Uri BuildOpenWeatherMapUri()
         {
-            StringBuilder stringBuilder = new StringBuilder(OpenWeatherMapURL);
-            stringBuilder.Append("?lat=");
-            stringBuilder.Append(lat);
-            return new Uri(stringBuilder.ToString());
+            var queryParams = new NameValueCollection();
+
+            // Add query parameters
+            queryParams.Add("lat", lat);
+            queryParams.Add("lon", lon);
+            queryParams.Add("units", units);
+
+            // Add the API key as a query parameter
+            queryParams.Add("appid", OpenWeatherMapKey);
+
+            // Build the complete URL with query parameters
+            var uriBuilder = new UriBuilder("/endpoint");
+            uriBuilder.Query = string.Join("&", queryParams.AllKeys
+                .Select(key => $"{key}={System.Web.HttpUtility.UrlEncode(queryParams[key])}"));
+
+            return uriBuilder.Uri;
+
         }
 
-
+        public async void GetTemperature(string lat, string lon)
+        {
+            Uri tempUri = BuildOpenWeatherMapUri();
+            var result = await _httpClient.GetAsync(tempUri).ConfigureAwait(false);
+        }
 
     }
 }
